80cc8802f1735b984945a9b4a52d65b29f56d909
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-27 14:32:41 +0500

    create video player filem, use export/inport

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 'use strict'
+import {
+    radioPlayerInit
+}
+from './radioPlayer.js';
+import {
+    musicPlayerInit
+}
+from './musicPlayer.js';
+import {
+    videoPlayerInit
+}
+from './videoPlayer.js';
+
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
