b91a3149a25c6e7d4fadf8063f4d8b2f52e1806d
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-27 14:48:32 +0500

    switch application between each other

--- scripts/index.js
+++ scripts/index.js
@@ -13,18 +13,20 @@ import {
 from './videoPlayer.js';
 
 const playerBtn = document.querySelectorAll('.player-btn'),
-    playerBlock = document.querySelectorAll('.player-block');
+    playerBlock = document.querySelectorAll('.player-block'),
+    temp = document.querySelector('.temp');
 
+const deactivationPlayer = () => {
+    temp.style.display = 'none';
+    playerBlock.forEach(elem => elem.classList.remove('active'))
+    playerBtn.forEach(elem => elem.classList.remove('active'))
+};
 
-
-
-playerBtn.forEach((elem, i) => {
-    elem.addEventListener('click', () => {
-        deactivationPlayer();
-        elem.classList.add('active');
-        playerBlock[i].classList.add('active');
-    });
-});
+playerBtn.forEach((elem, i) => elem.addEventListener('click', () => {
+    deactivationPlayer();
+    elem.classList.add('active');
+    playerBlock[i].classList.add('active');
+}));
 
 radioPlayerInit();
 videoPlayerInit();
