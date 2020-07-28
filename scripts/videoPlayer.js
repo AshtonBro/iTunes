5a36349c95ac29fe720ecd279b8b0b38d975e831
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-28 14:03:39 +0500

    toggle icon

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,10 +1,30 @@
 export const videoPlayerInit = () => {
-    const videoPlayer = document.querySelector('.video-player'),
-        videoButtonPlay = document.querySelector('.video-button__play'),
-        videoButtonStop = document.querySelector('.video-button__stop'),
-        videoTimePassed = document.querySelector('.video-time__passed'),
-        videoProgress = document.querySelector('.video-progress'),
-        videoTimeTotal = document.querySelector('.video-time__total');
+    const videoPlayer = document.querySelector(".video-player"),
+        videoButtonPlay = document.querySelector(".video-button__play"),
+        videoButtonStop = document.querySelector(".video-button__stop"),
+        videoTimePassed = document.querySelector(".video-time__passed"),
+        videoProgress = document.querySelector(".video-progress"),
+        videoTimeTotal = document.querySelector(".video-time__total");
 
+    const toggleIcon = () => {
+        if (videoPlayer.paused) {
+            videoButtonPlay.classList.remove("fa-pause");
+            videoButtonPlay.classList.add("fa-play");
+        } else {
+            videoButtonPlay.classList.add("fa-pause");
+            videoButtonPlay.classList.remove("fa-play");
+        }
+    };
 
+    const togglePlay = () => {
+        if (videoPlayer.paused) {
+            videoPlayer.play();
+        } else {
+            videoPlayer.pause();
+        }
+        toggleIcon();
+    }
+
+    videoPlayer.addEventListener("click", togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
 };
