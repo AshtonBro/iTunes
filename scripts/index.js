6e4e651e5729f667ae2de7ef5ef4798b1cdb7bd6
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-28 13:32:46 +0500

    get all elements from videoPlayer

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,25 @@
-'use strict'
-import {
-    radioPlayerInit
-}
-from './radioPlayer.js';
-import {
-    musicPlayerInit
-}
-from './musicPlayer.js';
-import {
-    videoPlayerInit
-}
-from './videoPlayer.js';
+"use strict";
+import { radioPlayerInit } from "./radioPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
 
-const playerBtn = document.querySelectorAll('.player-btn'),
-    playerBlock = document.querySelectorAll('.player-block'),
-    temp = document.querySelector('.temp');
+const playerBtn = document.querySelectorAll(".player-btn"),
+  playerBlock = document.querySelectorAll(".player-block"),
+  temp = document.querySelector(".temp");
 
 const deactivationPlayer = () => {
-    temp.style.display = 'none';
-    playerBlock.forEach(elem => elem.classList.remove('active'))
-    playerBtn.forEach(elem => elem.classList.remove('active'))
+  temp.style.display = "none";
+  playerBlock.forEach((elem) => elem.classList.remove("active"));
+  playerBtn.forEach((elem) => elem.classList.remove("active"));
 };
 
-playerBtn.forEach((elem, i) => elem.addEventListener('click', () => {
+playerBtn.forEach((elem, i) =>
+  elem.addEventListener("click", () => {
     deactivationPlayer();
-    elem.classList.add('active');
-    playerBlock[i].classList.add('active');
-}));
+    elem.classList.add("active");
+    playerBlock[i].classList.add("active");
+  })
+);
 
 radioPlayerInit();
 videoPlayerInit();
