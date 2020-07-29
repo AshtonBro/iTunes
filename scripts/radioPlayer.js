af303036e85cdcf7d1237b6ada49a8ab363ebc3e
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-29 22:24:03 +0500

    selectItem

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -12,16 +12,25 @@ export const radioPlayerInit = () => {
 
     const changeIconPlay = () => {
         if (audio.paused) {
+            radio.classList.remove('play')
             radioStop.classList.add('fa-play');
             radioStop.classList.remove('fa-stop');
         } else {
+            radio.classList.add('play')
             radioStop.classList.add('fa-stop');
             radioStop.classList.remove('fa-play');
         }
     };
 
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
     radioNavigation.addEventListener('change', event => {
         const target = event.target;
+        const parrent = target.closest('.radio-item');
+        selectItem(parrent);
+
         radioStop.disabled = false;
         audio.src = target.dataset.radioStantion;
 
