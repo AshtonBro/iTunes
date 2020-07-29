f3413d848582ce93bb1910345775c33a7922b3f5
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-29 22:15:23 +0500

    changeIconPlay

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -8,15 +8,33 @@ export const radioPlayerInit = () => {
 
     const audio = new Audio();
     audio.type = 'audio/acc';
-
     radioStop.disabled = true;
+
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else {
+            radioStop.classList.add('fa-stop');
+            radioStop.classList.remove('fa-play');
+        }
+    };
 
     radioNavigation.addEventListener('change', event => {
         const target = event.target;
+        radioStop.disabled = false;
         audio.src = target.dataset.radioStantion;
 
         audio.play();
+        changeIconPlay();
     });
 
-    radioStop
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        changeIconPlay();
+    });
 };
